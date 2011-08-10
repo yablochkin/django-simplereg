f2a3cd892117a7c4794717a4752b00a562a580bb
Bers <bers.com@gmail.com>
2011-08-10 21:15:05 +0300

    add registration callback

--- simplereg/views.py
+++ simplereg/views.py
@@ -7,7 +7,8 @@ from simplereg.forms import RegForm
 
 def registration(request, template_name='auth/registration.html',
             redirect_field_name=REDIRECT_FIELD_NAME,
-            form_class=RegForm, extra_context=None):
+            form_class=RegForm, extra_context=None,
+            callback=None):
 
     redirect_to = request.REQUEST.get(redirect_field_name, '')
 
@@ -16,6 +17,10 @@ def registration(request, template_name='auth/registration.html',
         user = form.save(request)
         user = authenticate(username=request.POST['email'], password=request.POST['password'])
         login(request, user)
+
+        if callback:
+            callback(request, user)
+
         return HttpResponseRedirect(redirect_to)
 
     return direct_to_template(request, template_name, {
