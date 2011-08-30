0c668c30c78a8b27ed0a3e710f45485e15557b26
Bers <bers.com@gmail.com>
2011-08-30 15:59:10 +0300

    autologin, extra_content fix

--- simplereg/views.py
+++ simplereg/views.py
@@ -8,21 +8,27 @@ from simplereg.forms import RegForm
 def registration(request, template_name='auth/registration.html',
             redirect_field_name=REDIRECT_FIELD_NAME,
             form_class=RegForm, extra_context=None,
-            callback=None):
+            callback=None, autologin=True):
 
     redirect_to = request.REQUEST.get(redirect_field_name, '')
 
     form = form_class(request.POST or None)
     if form.is_valid():
         user = form.save(request)
-        user = authenticate(username=request.POST['email'], password=request.POST['password'])
-        login(request, user)
+
+        if autologin:
+            user = authenticate(username=request.POST['email'], password=request.POST['password'])
+            login(request, user)
 
         if callback:
             callback(request, user)
 
         return HttpResponseRedirect(redirect_to)
 
+    context = {
+        'form': form
+    }
+    context.update(extra_context or {})
     return direct_to_template(request, template_name, {
             'form': form
         })
